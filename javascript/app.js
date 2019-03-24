30c4b727db418c22104118270e817e39fa57d11a
Markus Maasikas <markus@hotmail.com>
2019-03-23 20:28:14 -0400

    js updated

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,34 @@
 
+var authKey = "rtUoX0RChcNWQbZm4MbcrLrqpSCJ2MNh";
 
-  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=n6kd0XbtbGeBReBcPgcYH0yGX8nVo5uH";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
+var searchTerm = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
 
-  });
+
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=rtUoX0RChcNWQbZm4MbcrLrqpSCJ2MNh";
+
+
+//https:api.nytimes.com/svc/search/v2/articlesearch.json?
+//api-key=abcd&
+//q=qwer&
+//date=2019-03-23&
+//...
+
+  var articleCounter = 0;
+  function runQuery(numArticles, queryURL) {
+
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      for (var i = 0; i < numArticles; i++) {
+
+
+        articleCounter++;
+
+        var wellSection = $("<div>");
